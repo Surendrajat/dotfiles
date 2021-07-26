77b4f0248ec2d1fb21aba958d61f8e37303b591d
Surendrajat <surendrajat@protonmail.com>
2021-07-27 03:35:03 +0530

    [waybar]

--- .config/waybar/scripts/dunst.sh
+++ .config/waybar/scripts/dunst.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 COUNT=$(dunstctl count waiting)
-ENABLED=
-DISABLED=
-if [ $COUNT != 0 ]; then DISABLED=" $COUNT"; fi
+ENABLED=
+DISABLED=
+if [ $COUNT != 0 ]; then DISABLED=""; fi
 if dunstctl is-paused | grep -q "false" ; then echo $ENABLED; else echo $DISABLED; fi
 
