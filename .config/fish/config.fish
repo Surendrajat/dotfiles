a7a4836efb7001f5a6ed083e7017c3138f936875
Surendrajat <surendrajat@protonmail.com>
2021-09-25 20:24:04 +0530

    [fish] no default ls -al

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -137,7 +137,7 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias ls='exa --color=always --group-directories-first --icons' # preferred listing
 alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons'  # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
