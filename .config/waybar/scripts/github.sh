32b37ba19847ed69846b40c10bd5f3d1bbf269c8
Surendrajat <surendrajat@protonmail.com>
2021-10-29 17:11:00 +0530

    [waybar] custom weather module

--- .config/waybar/scripts/github.sh
+++ .config/waybar/scripts/github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN=$(cat ~/.config/gh-notif-token)
+TOKEN=$(cat $PRIV_ENV_FILE | grep gh-notif-token | cut -d'=' -f2)
 COUNT=$(curl -H "Authorization: token $TOKEN" https://api.github.com/notifications -s | jq length)
 # if [ $COUNT != 0 ]; then echo " $COUNT"; else echo ""; fi
 if [ $COUNT != 0 ]; then echo ""; fi
